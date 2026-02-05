8be869364078bb9da98849a4815bf25ca2d6e8d9
Tim Leach <4410524+TimLeach635@users.noreply.github.com>
2026-02-05 15:59:02 +0000

    wip: adding  struct

--- src/arc.rs
+++ src/arc.rs
@@ -15,13 +15,12 @@ pub struct Arc<T> {
 
 impl<T: Scalar + Zero + ClosedAddAssign + ClosedMulAssign + SimdComplexField> Arc<T> {
     pub fn line(&self) -> Line<T> {
-        let r_squared = self.radius * self.radius;
-        let a_squared = self.alpha * self.alpha;
-        let b_squared = self.beta * self.beta;
+        let r_squared = self.radius.clone() * self.radius.clone();
+        let a_squared = self.alpha.clone() * self.alpha.clone();
+        let b_squared = self.beta.clone() * self.beta.clone();
         let multiplier = r_squared / (a_squared + b_squared);
-        Line::from_normal(
-            &(multiplier * vector![self.alpha, self.beta]),
-            self.is_negative,
-        )
+        let normal: Vector2<T> = vector![self.alpha.clone(), self.beta.clone()];
+        let unit_normal: Vector2<T> = normal * multiplier;
+        Line::from_normal(&unit_normal, self.is_negative)
     }
 }
